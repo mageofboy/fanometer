ca5bdcf693de6a68334a3b06d09baf89bd1acf58
eric leong <you@example.com>
2018-11-17 23:21:44 -0800

    updated

--- webapp/cheers/views.py
+++ webapp/cheers/views.py
@@ -5,8 +5,11 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    # context = {'latest_question_list': latest_question_list}
+    context = {'table': [["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"], ["chocolate", "cats", "are dumb"]],
+                'chicken': ["meow"]}
     return render(request, 'cheers/index.html', context)
 
 def main(request):
-    return render(request, 'cheers/main.html')
+    context = {'bands': ["1","2","3","4","5","6","7","8","5654","6","5478","647","545r","436","547","5476","54","754","yrf","hf","hre","tre","tre","yre","ytr","y64","754","543","7657","535","2","5437","56u","y","r","htru","t4y","436","246","426","42"]}
+    return render(request, 'cheers/main.html', context)
